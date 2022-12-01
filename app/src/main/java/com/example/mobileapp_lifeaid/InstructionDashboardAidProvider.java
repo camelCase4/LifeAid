aa33df4670768e16a115b738d95c3f5c70e7e4a6
camelCase4 <rogerjasering@gmail.com>
2022-12-02 00:13:33 +0800

    second commit

---
+++ app/src/main/java/com/example/mobileapp_lifeaid/InstructionDashboardAidProvider.java
@@ -0,0 +1,14 @@
+package com.example.mobileapp_lifeaid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InstructionDashboardAidProvider extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instruction_dashboard_aid_provider);
+    }
+}
--- app/src/main/java/com/example/mobileapp_lifeaid/InstructionDashboardAidSeeker.java
+++ app/src/main/java/com/example/mobileapp_lifeaid/InstructionDashboardAidSeeker.java
@@ -7,22 +7,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class TrustedContact2Dashboard extends AppCompatActivity {
+public class InstructionDashboardAidSeeker extends AppCompatActivity {
 
     ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trusted_contact2_dashboard);
+        setContentView(R.layout.activity_instruction_dashboard_aid_seeker);
 
-        imageView = (ImageView)findViewById(R.id.img_buttonnext);
+        imageView = (ImageView) findViewById(R.id.img_next);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(TrustedContact2Dashboard.this, InstructionDashboardAidSeeker.class);
+                Intent intent = new Intent(InstructionDashboardAidSeeker.this,AidSeekerMainDash.class);
                 startActivity(intent);
             }
         });
+
+
 
     }
 }
