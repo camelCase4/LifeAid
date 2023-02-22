577a30beb5dcebe15a302a6b14e4f6f2277e4f29
camelCase4 <rogerjasering@gmail.com>
2023-02-22 12:46:01 +0800

    second commit

--- app/src/main/java/com/example/mobileapp_lifeaid/AidSeekerMainDash.java
+++ app/src/main/java/com/example/mobileapp_lifeaid/AidSeekerMainDash.java
@@ -1,14 +1,61 @@
 package com.example.mobileapp_lifeaid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class AidSeekerMainDash extends AppCompatActivity {
+// i added a new implements 2/22/2023
+public class AidSeekerMainDash extends AppCompatActivity implements LocationListener {
 
+    ImageView alertallbtn;
+    //checkpoint 2/22/20233
+    LocationManager lm;
+    //----
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aid_seeker_main_dash);
+
+        alertallbtn = (ImageView) findViewById(R.id.imageView34);
+
+        alertallbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getLoc();
+            }
+        });
     }
+
+    //checkpoint 2/22/2023
+    public void getLoc()
+    {
+        if(ContextCompat.checkSelfPermission(AidSeekerMainDash.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(AidSeekerMainDash.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},100);
+        }
+
+        lm = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,5,AidSeekerMainDash.this);
+
+
+
+    }
+    //-------
+    //checkpoint 2/22/2023
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        Toast.makeText(this, location.getLatitude()+", "+location.getLongitude(), Toast.LENGTH_SHORT).show();
+    }
+    //-------
 }
