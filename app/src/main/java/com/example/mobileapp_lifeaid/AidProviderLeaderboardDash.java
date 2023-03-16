bf2ad7b0c4d0fc830f965472d074737131e48aa9
camelCase4 <rogerjasering@gmail.com>
2023-03-16 23:00:45 +0800

    second commit

--- app/src/main/java/com/example/mobileapp_lifeaid/AidProviderLeaderboardDash.java
+++ app/src/main/java/com/example/mobileapp_lifeaid/AidProviderLeaderboardDash.java
@@ -3,12 +3,27 @@ package com.example.mobileapp_lifeaid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class AidProviderLeaderboardDash extends AppCompatActivity {
+
+    TextView leadProvs;
+
+    int providerCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aid_provider_leaderboard_dash);
+
+        //3/16/2023
+        leadProvs = (TextView) findViewById(R.id.leaders);
+
+        leadProvs.setMovementMethod(new ScrollingMovementMethod());
+        //---
+
     }
+
+    
 }
