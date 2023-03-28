13146f42d42d176c6535773a81f5a6024564c5e1
camelCase4 <rogerjasering@gmail.com>
2023-03-28 23:06:13 +0800

    second commit

--- app/src/main/java/com/example/mobileapp_lifeaid/AdminManageRecords.java
+++ app/src/main/java/com/example/mobileapp_lifeaid/AdminManageRecords.java
@@ -1,26 +1,52 @@
 package com.example.mobileapp_lifeaid;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AdminManageRecords extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     int role_holder = 0;
-    String chosenRole = "";
+    public static String chosenRole = "";
+    TextView conts,search;
+
+    public static String user_id ="";
+    public static String specifiedID = "";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_manage_records);
+
+        conts = (TextView) findViewById(R.id.contents);
+        search = (TextView) findViewById(R.id.srch);
 
 
 
@@ -33,6 +59,13 @@ public class AdminManageRecords extends AppCompatActivity implements AdapterView
         sp.setOnItemSelectedListener(this);
         sp.setSelection(role_holder);
 
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                specificSearch();
+            }
+        });
+
 
     }
 
@@ -43,11 +76,13 @@ public class AdminManageRecords extends AppCompatActivity implements AdapterView
 
         if(temp.equals("Aid-Provider"))
         {
-            Toast.makeText(AdminManageRecords.this,"Provider Selected",Toast.LENGTH_SHORT).show();
+            chosenRole = "Aid-Provider";
+            gettingUsers(chosenRole);
         }
         else if(temp.equals("Aid-Seeker"))
         {
-            Toast.makeText(AdminManageRecords.this,"Seeker Selected",Toast.LENGTH_SHORT).show();
+            chosenRole = "Aid-Seeker";
+            gettingUsers(chosenRole);
         }
 
     }
@@ -56,4 +91,107 @@ public class AdminManageRecords extends AppCompatActivity implements AdapterView
     public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
+
+    public void gettingUsers(String cr)
+    {
+        DatabaseReference dr = FirebaseDatabase.getInstance().getReference(cr);
+        dr.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+                conts.setText("");
+                for (DataSnapshot ds : datasnapshot.getChildren()) {
+                    String uid = ds.getKey();
+
+                    dr.child(uid).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DataSnapshot> task) {
+                            if(task.isSuccessful())
+                            {
+                                if(task.getResult().exists())
+                                {
+                                    DataSnapshot snaps = task.getResult();
+                                    String tempproviderUID = uid;
+                                    String con_num = String.valueOf(snaps.child("phonenum").getValue());
+                                    String fullName = String.valueOf(snaps.child("fname").getValue()) +" "+String.valueOf(snaps.child("lname").getValue());
+
+
+                                    String temp = con_num +"             "+tempproviderUID+"\n\n";
+                                    SpannableString ss = new SpannableString(temp);
+                                    ClickableSpan clickableSpan = new ClickableSpan() {
+
+                                        @Override
+                                        public void onClick(View textView) {
+
+
+                                        }
+                                        @Override
+                                        public void updateDrawState(TextPaint ds) {
+                                            super.updateDrawState(ds);
+                                            ds.setUnderlineText(false);
+                                        }
+                                    };
+                                    ss.setSpan(clickableSpan, 23, temp.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+
+                                    conts.append(ss);
+                                    conts.setMovementMethod(LinkMovementMethod.getInstance());
+                                    conts.setHighlightColor(Color.TRANSPARENT);
+
+
+                                }
+                                else
+                                {
+                                    Toast.makeText(AdminManageRecords.this, "Failed to read!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else
+                            {
+                                Toast.makeText(AdminManageRecords.this, "Task was not successful!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+
+                }
+
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+
+
+        });
+
+
+
+    }
+    //3/28/2023
+    public void specificSearch()
+    {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        final EditText edittext = new EditText(AdminManageRecords.this);
+        alert.setMessage("Enter a user UID");
+        alert.setTitle("F i n d   U s e r");
+
+        alert.setView(edittext);
+
+        alert.setPositiveButton("Proceed", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                specifiedID = edittext.getText().toString();
+                Toast.makeText(AdminManageRecords.this, specifiedID, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+            }
+        });
+
+        alert.show();
+    }
+    //--
 }
--- app/src/main/java/com/example/mobileapp_lifeaid/AidProviderClaimCert.java
+++ app/src/main/java/com/example/mobileapp_lifeaid/AidProviderClaimCert.java
@@ -2,8 +2,20 @@ package com.example.mobileapp_lifeaid;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PackageManagerCompat;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,7 +30,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.io.OutputStream;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class AidProviderClaimCert extends AppCompatActivity {
 
@@ -33,6 +47,8 @@ public class AidProviderClaimCert extends AppCompatActivity {
     String certificateURL = "";
 
     boolean flag = false;
+
+    private static final int REQUEST_CODE = 1; //3/28/2023
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,12 +95,79 @@ public class AidProviderClaimCert extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 updateProviderCannotClaim();
-                certificate.setImageResource(R.drawable.envelope);
+                //certificate.setImageResource(R.drawable.envelope);
+                //3/28/2023
+                if(ContextCompat.checkSelfPermission(AidProviderClaimCert.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+                {
+                    saveImage();
+                }
+                else
+                {
+                    ActivityCompat.requestPermissions(AidProviderClaimCert.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_CODE);
+                }
+                //---
+
             }
         });
 
 
     }
+    //3/28/2023
+    public void saveImage()
+    {
+        Uri image;
+        ContentResolver contentResolver = getContentResolver();
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+        {
+            image = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
+        }
+        else
+        {
+            image = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        }
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME,System.currentTimeMillis()+".jpg");
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE,"images/*");
+        Uri uri = contentResolver.insert(image,contentValues);
+
+        try{
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) certificate.getDrawable();
+            Bitmap bitmap = bitmapDrawable.getBitmap();
+
+            OutputStream outputStream = contentResolver.openOutputStream(Objects.requireNonNull(uri));
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100,outputStream);
+            Objects.requireNonNull(outputStream);
+
+            Toast.makeText(AidProviderClaimCert.this, "Downloaded! Thank you for your service!",Toast.LENGTH_SHORT).show();
+            certificate.setImageResource(R.drawable.envelope);
+
+        }catch (Exception e)
+        {
+
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == REQUEST_CODE)
+        {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                saveImage();
+            }
+            else
+            {
+                Toast.makeText(AidProviderClaimCert.this,"Please Accept The Permission!",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    //----
     public void gettingProviderData()
     {
         dr.addListenerForSingleValueEvent(new ValueEventListener() {
